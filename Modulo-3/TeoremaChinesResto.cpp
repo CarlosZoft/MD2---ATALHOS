fbc5fa81806204f76239116dda803e318610dc14
Carlos <carlosoftwar@gmail.com>
2021-05-14 13:30:35 -0300

    Update : melhorando visualização

--- Modulo-3/TeoremaChinesResto.cpp
+++ Modulo-3/TeoremaChinesResto.cpp
@@ -20,15 +20,22 @@ int main() {
   int A[100],M[100],B[100],B2[100],K[100],ABK[100]; 
   int Y = 1;
   int Soma = 0;
+  int qtd = 0;
+  cout << "São quantas equações ? " << endl;
+  cin >> qtd;
 
-  for(int i = 0 ; i < 4 ; ++i){
-    cout << " Digite o valor A e M da equação" << i+1 << "Exemplo: A M" << endl;
+
+
+  for(int i = 0 ; i < qtd ; ++i){
+    cout << "Digite os valores A e M da equação " << i+1 
+    << " " << "Exemplo: A M" << endl;
     cin >> A[i] >> M[i];
     Y *= M[i];
   }
-  cout << Y << endl;
+
   cout <<"A M B B' K ABK" << "\n";
-  for(int j = 0 ; j < 4 ; ++j){
+
+  for(int j = 0 ; j < qtd ; ++j){
     B[j] = int(Y / M[j]);
     B2[j] = B[j] % M[j];
     K[j] = imod(B2[j], M[j]);
